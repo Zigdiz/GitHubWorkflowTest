e3ff8e57616acc858d739beed21fc07ecdff1b0b
Tuomas Reijonen <hture01@xamk.fi>
2024-01-10 08:59:52 +0200

    swagger change

--- GitHubWorkflowTest/Program.cs
+++ GitHubWorkflowTest/Program.cs
@@ -17,11 +17,14 @@ namespace GitHubWorkflowTest
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
